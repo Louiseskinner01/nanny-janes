44727959e9e1210f462a9ac5bb06142035531d94
Louise sKinner <louise.skinner40@gmail.com>
2026-05-05 13:07:15 +0100

    Fixed parent-info content-section with new JS

--- assets/js/parent-info.js
+++ assets/js/parent-info.js
@@ -1,8 +1,16 @@
-
 function showSection(sectionId) {
+
+    // hide intro paragraph
+    const intro = document.getElementById('intro-p');
+    if (intro) {
+        intro.style.display = 'none';
+    }
+
+    // hide all sections
     document.querySelectorAll('.content-section').forEach(section => {
         section.classList.remove('active');
     });
 
+    // show selected section
     document.getElementById(sectionId).classList.add('active');
 }
